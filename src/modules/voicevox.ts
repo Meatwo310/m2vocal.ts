1bdd18ece65556de07faf0bc817091106e9f27c1
Meatwo310 <git@meatwo310.net>
2026-04-05 01:53:48 +0900

    feat: VC読み上げを実装

---
+++ src/modules/voicevox.ts
@@ -0,0 +1,42 @@
+import {type ArgsOf, Discord, On} from "discordx";
+import _VVClient from "voicevox-client";
+import {createAudioPlayer, createAudioResource, getVoiceConnection, StreamType} from "@discordjs/voice";
+import {Readable} from "node:stream";
+
+const VVClient = (_VVClient as any).default;
+
+@Discord()
+export class Voicevox {
+  @On()
+  async messageCreate([message]: ArgsOf<"messageCreate">): Promise<void> {
+    const guildId = message.guildId;
+    if (!guildId) {
+      return;
+    }
+
+    const voice = getVoiceConnection(guildId);
+    if (!voice) {
+      return;
+    }
+
+    const url = process.env.VOICEVOX_URL;
+    if (!url) {
+      console.warn(`VOICEVOX_URL not specified`);
+      return
+    }
+    const client: _VVClient = new VVClient(url);
+
+    const msg = message.content;
+
+    const query = await client.createAudioQuery(msg, 1);
+    const buf = await query.synthesis(1);
+
+    const player = createAudioPlayer();
+    const resource = createAudioResource(
+      Readable.from(Buffer.from(buf)),
+      { inputType: StreamType.Arbitrary }
+    );
+    player.play(resource);
+    voice.subscribe(player);
+  }
+}
