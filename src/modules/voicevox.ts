46a065c61ff358db7213488ea147d46468fdce0f
Meatwo310 <git@meatwo310.net>
2026-04-05 02:22:57 +0900

    feat: VV読み上げのエラーハンドリング

--- src/modules/voicevox.ts
+++ src/modules/voicevox.ts
@@ -26,19 +26,23 @@ export class Voicevox {
       console.warn(`VOICEVOX_URL not specified`);
       return
     }
-    const client: _VVClient = new VVClient(url);
 
+    const client: _VVClient = new VVClient(url);
     const msg = message.content;
 
-    const query = await client.createAudioQuery(msg, 1);
-    const buf = await query.synthesis(1);
+    try {
+      const query = await client.createAudioQuery(msg, 1);
+      const buf = await query.synthesis(1);
 
-    const player = createAudioPlayer();
-    const resource = createAudioResource(
-      Readable.from(Buffer.from(buf)),
-      { inputType: StreamType.Arbitrary }
-    );
-    player.play(resource);
-    voice.subscribe(player);
+      const player = createAudioPlayer();
+      const resource = createAudioResource(
+        Readable.from(Buffer.from(buf)),
+        {inputType: StreamType.Arbitrary}
+      );
+      player.play(resource);
+      voice.subscribe(player);
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
