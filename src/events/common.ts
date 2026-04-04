8652cf4acc1776cffe0f3970f912e309da82c783
Meatwo310 <git@meatwo310.net>
2026-04-04 21:32:07 +0900

    chore: 不要なイベントハンドラーを退避

--- src/events/common.ts
+++ src/events/common.ts
@@ -2,8 +2,8 @@ import { Discord, On, type ArgsOf } from "discordx";
 
 @Discord()
 export class Example {
-  @On()
-  messageCreate([message]: ArgsOf<"messageCreate">): void {
-    console.log(message.author.username, "said:", message.content);
-  }
+  // @On()
+  // messageCreate([message]: ArgsOf<"messageCreate">): void {
+  //   console.log(message.author.username, "said:", message.content);
+  // }
 }
